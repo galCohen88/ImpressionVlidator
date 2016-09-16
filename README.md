e6ccd4bf63efa87c6bbc3777c8629dffcfc49853
nevix <gal. nevis@gmail.com>
2016-09-16 20:00:48 +0300

    ..

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 **Usage of opencv pattern matching:**
 
 https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_template_matching/py_template_matching.html
+
+**dont be lazy**
+follow opencv installation
+http://www.pyimagesearch.com/2015/06/22/install-opencv-3-0-and-python-2-7-on-ubuntu/
--- validator.py
+++ validator.py
@@ -1,29 +1,14 @@
 import cv2
-from cv2 import cv
+import numpy as np
 
-method = cv.CV_TM_SQDIFF_NORMED
+img_rgb = cv2.imread('template.png')
+template = cv2.imread('pattern.png')
+h, w = template.shape[:-1]
 
-# Read the images from the file
-small_image = cv2.imread('small_image.png')
-large_image = cv2.imread('large_image.jpeg')
+res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+threshold = .8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (10, 247, 49), 2)
 
-result = cv2.matchTemplate(small_image, large_image, method)
-
-# We want the minimum squared difference
-mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-
-# Draw the rectangle:
-# Extract the coordinates of our best match
-MPx,MPy = mnLoc
-
-# Step 2: Get the size of the template. This is the same size as the match.
-trows,tcols = small_image.shape[:2]
-
-# Step 3: Draw the rectangle on large_image
-cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
-
-# Display the original image with the rectangle around the match.
-cv2.imshow('output',large_image)
-
-# The image is only displayed if we call this
-cv2.waitKey(0)
+cv2.imwrite('result.png', img_rgb)
